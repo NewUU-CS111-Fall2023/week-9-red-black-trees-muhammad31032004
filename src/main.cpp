9cf56c4fb319f68108b434c1e384ac4082d44582
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2023-12-12 19:22:15 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,16 @@
 
 #include <iostream>
 #include "task_1.h"
+#include "task_2.h"
+#include "task_3.h"
+#include "task_4.h"
+#include "task_5.h"
+#include "task_6.h"
+#include "task_7.h"
+#include "task_8.h"
+#include "task_9.h"
+#include "task_10.h"
+
 
 int main() {
     std::cout << "Task 1" << std::endl;
@@ -18,5 +28,15 @@ int main() {
     // call for task 4
     std::cout << "Task 5" << std::endl;
     // call for task 5
+    std::cout << "Task 6" << std::endl;
+    // call for task 6
+    std::cout << "Task 7" << std::endl;
+    // call for task 7
+    std::cout << "Task 8" << std::endl;
+    // call for task 8
+    std::cout << "Task 9" << std::endl;
+    // call for task 9
+    std::cout << "Task 10" << std::endl;
+    // call for task 10
     return 0;
 }
